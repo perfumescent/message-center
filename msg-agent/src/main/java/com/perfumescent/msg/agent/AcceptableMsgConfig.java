40439f902f04c131d705c25421c0259546f2899b
yibowen <yibowen@cestc.cn>
2024-04-10 17:45:31 +0800

    t

---
+++ msg-agent/src/main/java/com/perfumescent/msg/agent/AcceptableMsgConfig.java
@@ -0,0 +1,7 @@
+package com.perfumescent.msg.agent;
+
+public interface AcceptableMsgConfig {
+    
+    void registerAcceptableMsgType();
+    void registerAcceptableMsgSender();
+}
---
+++ msg-broker/src/main/java/com/perfumescent/msg/broker/ConsumableMsgConfig.java
@@ -0,0 +1,7 @@
+package com.perfumescent.msg.broker;
+
+public interface ConsumableMsgConfig {
+        
+        void registerConsumableMsgType();
+        void registerConsumableMsgReceiver();
+}
